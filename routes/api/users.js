7a11efd3163974a771dc613d6bd0dec3e997b1a6
“Richard Middleton” <Richard@richardmiddleton.me>
2018-10-26 18:24:48 +0200

    jwt webtokens done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,11 @@
+const keys = require('../../config/keys');
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 // get api/users/test
 // tests users route
@@ -66,14 +69,42 @@ router.post('/login', (req, res) => {
                 .compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch) {
-                        res.json({msg: 'Success'});
+                        // User Matched
+                        const payload = {
+                            id: user.id,
+                            name: user.name,
+                            avatar: user.avatar
+                        }
+                        // Sign Token
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            {expiresIn: 3600},
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                })
+                            });
                     }
                     else {
                         return res.status(400).json({password: 'Password incorrect'})
                     }
-                })
-        })
-})
+                });
+        });
+});
 
+
+// post api/users/current
+// return current user
+// private
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email,
+        avatar: req.user.avatar
+    });
+})
 
 module.exports = router;
